0c3c82891e6bc3805626e7b4d440b2917d54ad86
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-08-30 15:32:04 +0530

    Added: Employee Details

---
+++ Employee.js
@@ -0,0 +1,10 @@
+//UC1
+const IS_ABSENT = 0
+
+let empCheck = Mat.floor(Math.random() * 10) %2;
+if (empCheck == IS_Absent) {
+    console.log("Emplyoyee is Absent");
+    return;
+} else {
+    console.log("Emplyoyee is Present");
+}
